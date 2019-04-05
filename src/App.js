ed90203fd2471c3ef12698587bff617e162c24c7
“halalan” <>
2019-04-05 22:22:43 +0700

    finall Passing Props

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ class App extends Component {
         <NavBar />
         <div className="content">
         <Switch>
-        <Route path="/Products" component ={Products} />
+        <Route path="/Products" 
+        render={props => <Products sortBy="newest"{...props}/>}
+        />
         <Route path="/posts" component ={Posts} />
         <Route path="/admin" component ={Dashboard} />
        <Route path="/" component ={Home} />
